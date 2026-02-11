34ceb725af48afe97fe097fd75f11328abe9ff99
Lakshika Padmamali <lakshi.padmamali@gmail.com>
2026-02-11 08:36:59 +0700

    Create schema.py

---
+++ fpga_cost_model_v1/src/schema.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+COMMON_COLS = [
+    "variant",
+    "op_type",
+    "IN","OUT","C","L","K",
+    "UNROLL","UNROLL_C","II",
+    "rank_r",
+    "baud","f_clk_mhz",
+    "bytes_in","bytes_out","bytes_total",
+    "macs","cycles_est",
+    "transform_cycles_est",
+    "resource_proxy_dsp",
+    "resource_proxy_bram",
+    "y_fpga_est_io_ms","y_fpga_est_compute_ms","y_fpga_est_transform_ms","y_fpga_est_total_ms",
+]
