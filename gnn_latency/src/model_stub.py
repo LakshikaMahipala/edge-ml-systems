852e84d7eda414c87620cf50eac607cb6e8cc845
Lakshika Padmamali <lakshi.padmamali@gmail.com>
2026-02-20 16:11:44 +0700

    Create model_stub.py

---
+++ gnn_latency/src/model_stub.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+class GNNSurrogateModel:
+    """
+    Stub placeholder.
+    Later: implement with PyTorch Geometric / DGL / custom message passing.
+    """
+    def __init__(self):
+        pass
+
+    def forward(self, graph_features):
+        raise NotImplementedError("Week 15 Day 3 will implement training model.")
