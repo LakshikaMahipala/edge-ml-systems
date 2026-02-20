62cc9428bb01fa97cf6ed3c5d65b4d4d582a0bd7
Lakshika Padmamali <lakshi.padmamali@gmail.com>
2026-02-20 07:00:39 +0700

    Update ops.py

--- darts/darts_impl/src/ops.py
+++ darts/darts_impl/src/ops.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class ReLUConvBN(nn.Module):
     def __init__(self, C_in, C_out, kernel_size, stride, padding):
@@ -12,13 +14,9 @@ class ReLUConvBN(nn.Module):
     def forward(self, x): return self.op(x)
 
 class Identity(nn.Module):
-    def __init__(self): super().__init__()
     def forward(self, x): return x
 
 class FactorizedReduce(nn.Module):
-    """
-    Downsample + channel adjust (used when stride=2 for skip).
-    """
     def __init__(self, C_in, C_out):
         super().__init__()
         assert C_out % 2 == 0
@@ -29,7 +27,13 @@ class FactorizedReduce(nn.Module):
 
     def forward(self, x):
         x = self.relu(x)
-        out = nn.functional.pad(x, (0,1,0,1))
-        y1 = self.conv1(out[:, :, 1:, 1:])
-        y2 = self.conv2(out[:, :, :-1, :-1])
-        return self.bn(nn.functional.relu(nn.functional.pad(y1, (0,0,0,0)) , inplace=True).new_tensor(0) + 0*y2) if False else self.bn(nn.functional.relu(nn.functional.pad(y1, (0,0,0,0)), inplace=True)*0 + y1*0 + 0*y2)  # dummy-safe
+        y1 = self.conv1(x)
+        y2 = self.conv2(x[:, :, 1:, 1:])
+        return self.bn(torch.cat([y1, y2], dim=1))
+
+def make_candidate_ops(C_in, C_out, stride):
+    return {
+        "skip": Identity() if stride == 1 and C_in == C_out else FactorizedReduce(C_in, C_out),
+        "conv3": ReLUConvBN(C_in, C_out, 3, stride=stride, padding=1),
+        "conv5": ReLUConvBN(C_in, C_out, 5, stride=stride, padding=2),
+    }
